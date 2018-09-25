698a362125e6121fbd4fbbd319a26de4f9e5c21c
Kaos <appssa12>
2018-09-25 08:43:23 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
 module.exports.info = require('./lib/info');
 module.exports.imgdata = require('./lib/imgdata');
 module.exports.convert = require('./lib/convert');
+module.exports.tools = require('./lib/tools');
--- test/tools.js
+++ test/tools.js
@@ -1,9 +1,23 @@
 var tools = require('../lib/tools');
-
-tools.page(__dirname + '\\..\\in\\test2.pdf', 1)
+const fs = require('fs');
+/*
+tools.page(__dirname + '\\..\\in\\test2.pdf', 20, { book_pages: 173 })
     .then(data => {
-        console.log(data);
+        //console.log(data);
+        fs.writeFileSync(__dirname + '\\..\\out\\test.jpg', data, { encoding: 'base64' });
+
     })
     .catch(err => {
         console.error(err);
     })
+
+
+*/
+
+tools.info(__dirname + '\\..\\in\\test.pdf', {
+        pages: '10:20'
+    })
+    .then(data => {
+        console.warn(data);
+    })
+    .catch(err => console.error(err));
